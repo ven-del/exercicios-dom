d2064e2f7d20f0dd6b60ccd113e71e4f583005d1
Wendell Régis <r.wendell.regis@gmail.com>
2025-05-13 18:38:06 -0300

    Adicionados dois botões que chamam duas funções
    O primeiro botão adicionado chama a função "mostrarDiaDaSemana", que utiliza o objeto Date para criar uma lógica para exibir o dia da semana.
    O segundo botão chama a função "descobrirNomeDev", que utiliza o objeto nomeDevMeses para artibuir uma lógica com base na condição. Também há uma condição especial para um dia específico do mês de janeiro.
    Ainda falta corrigir o problema de retornar "null" ao tentar modificar o innerHtml do span de id "texto-resultado"

--- app.js
+++ app.js
@@ -5,55 +5,36 @@
 // ***anos bissextos
 
 const objetoMeses = {
-    1: {
-        mes: 'Janeiro',
-        dias: 31
-    },
-    2: {
-        mes: 'Fevereiro',
-        dias: 28
-    },
-    3: {
-        mes: 'Março',
-        dias: 31
-    },
-    4: {
-        mes: 'Abril',
-        dias: 30
-    },
-    5: {
-        mes: 'Maio',
-        dias: 31
-    },
-    6: {
-        mes: 'Junho',
-        dias: 30
-    },
-    7: {
-        mes: 'Julho',
-        dias: 31
-    },
-    8: {
-        mes: 'Agosto',
-        dias: 31
-    },
-    9: {
-        mes: 'Setembro',
-        dias: 30
-    },
-    10: {
-        mes: 'Outubro',
-        dias: 31
-    },
-    11: {
-        mes: 'Novembro',
-        dias: 30
-    },
-    12: {
-        mes: 'Dezembro',
-        dias: 31
-    },
+    1: { mes: 'Janeiro', dias: 31 },
+    2: { mes: 'Fevereiro', dias: 28 },
+    3: { mes: 'Março', dias: 31 },
+    4: { mes: 'Abril', dias: 30 },
+    5: { mes: 'Maio', dias: 31 },
+    6: { mes: 'Junho', dias: 30 },
+    7: { mes: 'Julho', dias: 31 },
+    8: { mes: 'Agosto', dias: 31 },
+    9: { mes: 'Setembro', dias: 30 },
+    10: { mes: 'Outubro', dias: 31 },
+    11: { mes: 'Novembro', dias: 30 },
+    12: { mes: 'Dezembro', dias: 31 },
 }
+
+const nomeDevMeses = {
+    1: 'Bugado(a)',
+    2: 'Do CTRL, CTRL V',
+    3: 'Das gambiarras',
+    4: 'Que culpa o cache',
+    5: 'Que esquece o que faz',
+    6: 'Do git vazio',
+    7: 'Das try/ catch vazia',
+    8: 'Famosinho do LinkedIn',
+    9: 'Caçador de bugs',
+    10: 'Do Windows Pirata',
+    11: 'Do update sem where',
+    12: 'Do commit bugado',
+};
+
+
 const gerarValoresDosAnos = () => {
     const ano = document.getElementById('ano');
     ano.innerHTML = '';
@@ -79,11 +60,7 @@ const gerarValoresDosMeses = () => {
 }
 
 const checarAnoBissexto = (ano) => {
-    if (ano % 400 === 0 || (ano % 4 === 0 && ano % 100 !== 0)) {
-        return true;
-    } else {
-        return false;
-    }
+    return (ano % 400 === 0 || (ano % 4 === 0 && ano % 100 !== 0));
 }
 
 const gerarValoresDosDias = () => {
@@ -112,4 +89,63 @@ const gerarValoresDosDias = () => {
 
 const atualizarDias = () => {
     gerarValoresDosDias();
+};
+
+const mostrarDiaDaSemana = () => {
+    const diaSelecionado = Number(document.getElementById('dia').value);
+    const mesSelecionado = Number(document.getElementById('mes').value) - 1;
+    const anoSelecionado = Number(document.getElementById('ano').value);
+    const mensagemResultado = document.getElementById('mensagem-resultado');
+    const textoResultado = document.getElementById('texto-resultado');
+
+    if (isNaN(diaSelecionado) || isNaN(mesSelecionado) || isNaN(anoSelecionado) ||
+        diaSelecionado < 1 || diaSelecionado > 31 || mesSelecionado < 0 || mesSelecionado > 11) {
+        textoResultado.innerHTML = "Por favor, selecione uma data válida.";
+        return;
+    }
+
+    const data = new Date(anoSelecionado, mesSelecionado, diaSelecionado);
+    const diasDaSemana = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+    const diaDaSemana = diasDaSemana[data.getDay()];
+
+    mensagemResultado.innerHTML = ""; 
+    mensagemResultado.innerHTML = "O dia da semana é:"; 
+    textoResultado.innerHTML = ` ${diaDaSemana}`;
+};
+
+const descobrirNomeDev = () => {
+    const diaSelecionado = Number(document.getElementById('dia').value);
+    const mesSelecionado = Number(document.getElementById('mes').value);
+    const mensagemResultado = document.getElementById("mensagem-resultado");
+    const textoResultado = document.getElementById("texto-resultado");
+
+    if (isNaN(diaSelecionado) || isNaN(mesSelecionado) ||
+        diaSelecionado < 1 || diaSelecionado > 31 || mesSelecionado < 1 || mesSelecionado > 12) {
+        textoResultado.innerHTML = "Por favor, selecione uma data válida.";
+        return;
+    }
+
+    let primeiraParte = '';
+    let segundaParte = '';
+    const ultimoDigitoDia = diaSelecionado % 10;
+    if (ultimoDigitoDia === 5 && mesSelecionado === 1){
+        primeiraParte = 'Game Dev';
+        segundaParte = 'Fã de Linkin Park';
+    } else {
+        if (ultimoDigitoDia === 1 || ultimoDigitoDia === 2) {
+            primeiraParte = 'Desenvolvedor(a)';
+        } else if (ultimoDigitoDia === 3 || ultimoDigitoDia === 4 || ultimoDigitoDia === 5) {
+            primeiraParte = 'Programador(a)';
+        } else if (ultimoDigitoDia === 6 || ultimoDigitoDia === 7 || ultimoDigitoDia === 8) {
+            primeiraParte = 'Estagiário(a)';
+        } else if (ultimoDigitoDia === 9 || ultimoDigitoDia === 0) {
+            primeiraParte = 'Senior';
+        }
+    }
+
+    segundaParte = nomeDevMeses[mesSelecionado];
+
+    mensagemResultado.innerHTML = "";
+    mensagemResultado.innerHTML = "Seu nome de Dev é:";
+    textoResultado.innerHTML = ` ${primeiraParte} ${segundaParte}`;
 };
